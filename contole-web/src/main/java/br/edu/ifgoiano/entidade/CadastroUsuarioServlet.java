ee5701cea6a4b3ba6af60d3b6a23ad5828b78204
2021108102540065 <2021108102540065@lab01-23.ifgoiano.local>
2023-03-21 10:32:37 -0300

    parte 1 da aula, criação do servelet, verificarndo se a senhas estão iguais. Inclusão do servlet para o fontend frontCdastroUsuario.

---
+++ contole-web/src/main/java/br/edu/ifgoiano/entidade/CadastroUsuarioServlet.java
@@ -0,0 +1,33 @@
+package br.edu.ifgoiano.entidade;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cadastrarUsuario")
+public class CadastroUsuarioServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String senha1 = req.getParameter("senha1");
+		String senha2 = req.getParameter("senha2");
+		
+		//verificar se as senhas são iguais
+		if (senha1.equals(senha2)) {
+			//redirecionar o usuário para a página de login
+				resp.sendRedirect("index.html");
+		}else {
+			//redirecionar o usuário para a mesma página de cadátro de usuário
+				req.getRequestDispatcher("frontCadastroUsuario.html").forward(req, resp);
+		}
+		
+		
+		
+	}
+	
+	
+}
