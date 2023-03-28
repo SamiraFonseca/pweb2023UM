f3cdfa596b7cc2bc369a187c4c47d0224694d313
2021108102540065 <2021108102540065@lab01-23.ifgoiano.local>
2023-03-28 10:37:01 -0300

    Testes, simulando um banco de dados, usando o doGet pra saber como é (listar os usuários no console) já que usamos o doPost. Iniciou o uso do bootstrap no html.

--- contole-web/src/main/java/br/edu/ifgoiano/entidade/CadastroUsuarioServlet.java
+++ contole-web/src/main/java/br/edu/ifgoiano/entidade/CadastroUsuarioServlet.java
@@ -1,6 +1,8 @@
 package br.edu.ifgoiano.entidade;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/cadastrarUsuario")
 public class CadastroUsuarioServlet extends HttpServlet {
 
+	//Simulando banco de dados
+	private List<usuario> lstDeUsuario;
+	
+	@Override
+	public void init() throws ServletException {
+		this.lstDeUsuario = new ArrayList<usuario>();
+	}
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String senha1 = req.getParameter("senha1");
@@ -18,6 +28,13 @@ public class CadastroUsuarioServlet extends HttpServlet {
 		
 		//verificar se as senhas são iguais
 		if (senha1.equals(senha2)) {
+			usuario usu = new usuario();
+			usu.setNome(req.getParameter("nome"));
+			usu.setEmail(req.getParameter("email"));
+			usu.setSenha(senha1);
+			
+			lstDeUsuario.add(usu);
+			
 			//redirecionar o usuário para a página de login
 				resp.sendRedirect("index.html");
 		}else {
@@ -26,8 +43,22 @@ public class CadastroUsuarioServlet extends HttpServlet {
 		}
 		
 		
-		
 	}
+	
+	@Override
+	public void destroy() {
+		this.lstDeUsuario.clear();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		for (usuario usuario : lstDeUsuario) {
+			System.out.println(usuario.getNome().concat("-").concat(usuario.getEmail()));
+		}
+	}
+	
+	
+	
 	
 	
 }
